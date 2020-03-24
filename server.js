71008cc997d0dc14e8b21b2c1cb253f149227074
ibadeeCodes <ibadeeshaikh@gmail.com>
2020-03-24 14:28:33 -0700

    initial

---
+++ server.js
@@ -0,0 +1,75 @@
+const axios = require('axios')
+const express = require('express')
+const app = express()
+const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
+const flash = require('connect-flash')
+const session = require('express-session')
+const path = require('path')
+const moment = require('moment')
+
+const PORT = process.env.PORT || 5000
+
+app.engine('handlebars', exphbs())
+app.set('view engine', 'handlebars')
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.use(express.static(path.join(__dirname, '/public')))
+
+app.use(
+  session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+  })
+)
+
+app.use(flash())
+app.use(function(req, res, next) {
+  res.locals.success_msg = req.flash('success_msg')
+  res.locals.error_msg = req.flash('error_msg')
+  next()
+})
+
+app.listen(5000, console.log('server started at 5000'))
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.post('/', (req, res) => {
+  let country = req.body.country
+  if (country) {
+    country = country.charAt(0).toUpperCase() + country.substring(1)
+    console.log(country)
+    axios({
+      method: 'GET',
+      url: `https://covid-19-coronavirus-statistics.p.rapidapi.com/v1/stats?country=${country}`,
+      headers: {
+        'content-type': 'application/octet-stream',
+        'x-rapidapi-host': 'covid-19-coronavirus-statistics.p.rapidapi.com',
+        'x-rapidapi-key': '990d8d177fmsh097d636a87f4660p1a5355jsn8c22cc52c0b1'
+      }
+    })
+      .then(response => {
+        let data = response.data
+        // let date = data.data.lastChecked
+        if (data.message != 'OK') {
+          req.flash('error_msg', 'Country not found!')
+          return res.redirect('/')
+        }
+        return res.render('index', {
+          data: data.data,
+          time: moment(data.data.lastChecked).fromNow()
+        })
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  } else {
+    req.flash('error_msg', 'Country not found!')
+    res.redirect('/')
+  }
+})
